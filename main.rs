ae99ebb3a92bc275ecbc88e7678e1cef60f24695
JvFern4ndes <jvfernandes.contato@gmail.com>
2025-03-03 08:45:00 -0300

    feat: understanding multiple values on rust

---
+++ main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let notas: [f32, 4] = [10f32, 8f32, 9.5, 6.0];
+
+    println!(
+        "Nota1 = {}, Nota2 = {}, Nota3 = {}, Nota4 = {}",
+        notas[0],
+        notas[1],
+        notas[2],
+        notas[3]
+    );
+}
